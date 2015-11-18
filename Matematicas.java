31e0e2c97ec835aaff0b879238bef49d8b1832a1
Juan Martinez <jmmedina93@gmail.com>
2015-11-18 14:54:39 +0100

    Matematicas.java contiene el metodo 	modificado: Matematicas.java 	modificado: TestMatematicas.java

--- Matematicas.java
+++ Matematicas.java
@@ -1,16 +1,16 @@
-/*
-
+/**@author Juan Martinez
+ * @version 0.0
  */
 
 
 public class Matematicas {
-	
+	/**metodo que devuelve si un numero es par o no
+	 * @param par entero
+	 * @return boolean 
+	 * */
 	public static boolean esPar (int par) {
-		boolean espar=false;
-		if(par % 2==0)
-			espar=true;
-			
-		return esPar;
+		
+		return par % 2 == 0;
 	}
 }
 
--- TestMatematicas.java
+++ TestMatematicas.java
@@ -1,15 +1,17 @@
 /*
  * TestMatematicas.java
- *
- * 
- * 
  */
+ import java.util.Scanner;
 
-
-public class TestMatematicas.java {
+public class TestMatematicas {
 	
 	public static void main (String args[]) {
-		
+		//Iniciamos el Scanner
+		Scanner sc = new Scanner(System.in);
+		//Leemos el numero
+		System.out.println("Introduce numero entero");
+		int numero =sc.nextInt();
+		System.out.printf("El numero %d es par: %b\n",numero,Matematicas.esPar(numero));
 	}
 }
 
